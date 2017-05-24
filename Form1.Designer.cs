e10463576144509ffb707b8bccc7b8a4382423e5
student <student@NANO-13>
2017-05-24 11:22:45 +0200

    wersja ostateczna

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Start = new System.Windows.Forms.Button();
+            this.Stop = new System.Windows.Forms.Button();
+            this.port_p = new System.Windows.Forms.NumericUpDown();
+            this.Adres = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.port_p)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(58, 342);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(247, 49);
+            this.Start.TabIndex = 0;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            // 
+            // Stop
+            // 
+            this.Stop.Location = new System.Drawing.Point(418, 342);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(251, 49);
+            this.Stop.TabIndex = 1;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = true;
+            // 
+            // port_p
+            // 
+            this.port_p.Location = new System.Drawing.Point(449, 24);
+            this.port_p.Maximum = new decimal(new int[] {
+            65535,
+            0,
+            0,
+            0});
+            this.port_p.Name = "port_p";
+            this.port_p.Size = new System.Drawing.Size(253, 20);
+            this.port_p.TabIndex = 3;
+            // 
+            // Adres
+            // 
+            this.Adres.Location = new System.Drawing.Point(58, 24);
+            this.Adres.Name = "Adres";
+            this.Adres.Size = new System.Drawing.Size(246, 20);
+            this.Adres.TabIndex = 4;
+            this.Adres.Text = "Adres";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(58, 78);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(626, 225);
+            this.listBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(735, 403);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Adres);
+            this.Controls.Add(this.port_p);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.Start);
+            this.Name = "Form1";
             this.Text = "Form1";
+           ((System.ComponentModel.ISupportInitialize)(this.port_p)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.NumericUpDown port_p;
+        private System.Windows.Forms.TextBox Adres;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
 
 namespace SerwerTCP
 {
@@ -16,5 +18,56 @@ namespace SerwerTCP
         {
             InitializeComponent();
         }
+        private TcpListener serwer = null;
+        private TcpClient klient = null;
+
+        private void przycisk_start(object sender, EventArgs e)
+        {
+            IPAddress adresIP = null;
+            try
+            {
+                adresIP = IPAddress.Parse(Adres.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Bledny format adresu IP", "Blad");
+                Adres.Text = String.Empty;
+                return;
+            }
+
+
+            int port = System.Convert.ToInt32(port_p.Value);
+            try
+            {
+                serwer = new TcpListener(adresIP, port);
+                serwer.Start();
+
+                klient = serwer.AcceptTcpClient();
+                listBox1.Items.Add("Nawiazano polaczenia !");
+
+                Start.Enabled = false;
+                Stop.Enabled = true;
+
+                klient.Close();
+                serwer.Stop();
+            }
+            catch (Exception ex)
+            {
+                listBox1.Items.Add("Blad inicjacji serwera !");
+                MessageBox.Show(ex.ToString(), "Blad");
+            }
+        }
+            private void przycisk_stop_Click(object sender , EventArgs e)
+        {
+            serwer.Stop();
+            klient.Close();
+
+            listBox1.Items.Add("Zakonczono prace serwera !");
+
+            Start.Enabled = true;
+            Stop.Enabled = false;
+        }
+
+            
     }
 }
